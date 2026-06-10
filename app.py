39c2ac832a8fb9ce63319af718f5bb9cc1fb2b1d
Alessandra Lira <105083697+AlleLira@users.noreply.github.com>
2026-06-10 16:34:48 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Banco Imobiliário",
+    layout="wide"
+)
+
+st.title("🏦 Banco Imobiliário")
+
+st.write("""
+Utilize o menu lateral para navegar pelo sistema.
+""")
